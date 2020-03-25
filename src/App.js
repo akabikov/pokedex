40ccd968adebc19a938d24c4b5bb75a2834a6fe5
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-25 20:54:15 +0300

    Add Pokegame component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Pokedex from "./Pokedex"
+import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
 
 function App() {
   return (
-    <Pokedex />
+    <Pokegame />
   );
 }
 
---
+++ src/Pokegame.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pokedex from "./Pokedex";
+import pokemons from "./pokemons";
+
+class Pokegame extends React.Component {
+    render() {
+        return (
+            <Pokedex />
+        );
+    }
+}
+
+export default Pokegame;
