b6a1822e9e7578def38d0d0c2da965955eeb6a26
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-26 16:36:55 +0300

    Implement shuffling cards in the Pokegame component

--- src/Pokegame.jsx
+++ src/Pokegame.jsx
@@ -3,9 +3,30 @@ import Pokedex from "./Pokedex";
 import pokemons from "./pokemons";
 
 class Pokegame extends React.Component {
+    dealCards(array) {
+        let shuffled = [...array];
+
+        for(let i = shuffled.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+
+        if (shuffled.length % 2 > 0) shuffled.pop();
+
+        let delimeter = shuffled.length / 2;
+
+        return [shuffled.slice(0, delimeter), 
+                shuffled.slice(delimeter)];
+    }
+
     render() {
+        let hands = this.dealCards(pokemons);
+
         return (
-            <Pokedex />
+            <div>
+                <Pokedex pokemons={hands[0]} />;
+                <Pokedex pokemons={hands[1]} />;
+            </div>
         );
     }
 }
