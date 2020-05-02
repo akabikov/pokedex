68e91dd78cea6a04b8c9007952c9b3529505ea23
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-05-02 17:39:03 +0300

    Updates

---
+++ precache-manifest.dd6ac2951c008492d81b87e57928459b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578579335edccb265e523d7949d37eb4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "72305f0b1c55dcc71b1d",
+    "url": "/pokedex/static/css/main.3fcfe0b5.chunk.css"
+  },
+  {
+    "revision": "0d003a3f1ac537cfd967",
+    "url": "/pokedex/static/js/2.aa130717.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.aa130717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72305f0b1c55dcc71b1d",
+    "url": "/pokedex/static/js/main.8a43cdc8.chunk.js"
+  },
+  {
+    "revision": "e9c8d32f045e9b89c48b",
+    "url": "/pokedex/static/js/runtime-main.54804aa2.js"
+  }
+]);
